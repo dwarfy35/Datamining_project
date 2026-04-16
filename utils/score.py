33b788aacbb9edff0f57e5e13f86a998835cd480
3lv <cristian.vlad.vladutu@gmail.com>
2026-04-16 14:01:58 +0200

    Simpler structure

--- utils/score.py
+++ utils/score.py
@@ -5,7 +5,7 @@ from utils.constants import league_to_region_dict
 from utils.constants import league_to_continent_dict
 
 # calculate the normalized mutual information score for the predicted labels compared to to players labelled by the continent theyve played the most in
-def nmi_continents(labels_pred, uniq_player_ids, data):
+def score_continents(labels_pred, uniq_player_ids, data, score_fn):
 
     player_most_played_continent = data.groupby("playerid")["league"].agg(lambda x: x.map(lambda league: league_to_continent_dict[league]).mode().iloc[0] if not x.mode().empty else None)
     assert not player_most_played_continent.isna().any()
@@ -14,11 +14,11 @@ def nmi_continents(labels_pred, uniq_player_ids, data):
     uniq_player_ids_most_played_continent = np.vectorize(lambda x: player_most_played_continent[x])(uniq_player_ids)
     _, continent_labels = np.unique(uniq_player_ids_most_played_continent, return_inverse=True)
 
-    nmi_score = normalized_mutual_info_score(continent_labels, labels_pred)
-    return nmi_score
+    score = score_fn(continent_labels, labels_pred)
+    return score
 
 # calculate the normalized mutual information score for the predicted labels compared to to players labelled by the lol esports region theyve played the most in
-def nmi_regions(labels_pred, uniq_player_ids, data):
+def score_regions(labels_pred, uniq_player_ids, data, score_fn):
 
     player_most_played_region = data.groupby("playerid")["league"].agg(lambda x: x.map(lambda league: league_to_region_dict[league]).mode().iloc[0] if not x.mode().empty else None)
     assert not player_most_played_region.isna().any()
@@ -27,11 +27,11 @@ def nmi_regions(labels_pred, uniq_player_ids, data):
     uniq_player_ids_most_played_region = np.vectorize(lambda x: player_most_played_region[x])(uniq_player_ids)
     _, region_labels = np.unique(uniq_player_ids_most_played_region, return_inverse=True)
 
-    nmi_score = normalized_mutual_info_score(region_labels, labels_pred)
-    return nmi_score
+    score = score_fn(region_labels, labels_pred)
+    return score
 
 # calculate the normalized mutual information score for the predicted labels compared to to players labelled by the league theyve played the most in 
-def nmi_leagues(labels_pred, uniq_player_ids, data):
+def score_leagues(labels_pred, uniq_player_ids, data, score_fn):
     player_most_played_league = data.groupby("playerid")["league"].agg(lambda x: x.mode().iloc[0] if not x.mode().empty else None)
     assert not player_most_played_league.isna().any()
 
@@ -42,8 +42,8 @@ def nmi_leagues(labels_pred, uniq_player_ids, data):
     uniq_player_ids_most_played_league = np.vectorize(lambda x: player_most_played_league[x])(uniq_player_ids) 
     _, league_labels = np.unique(uniq_player_ids_most_played_league, return_inverse=True)
 
-    nmi_score = normalized_mutual_info_score(league_labels, labels_pred)
-    return nmi_score
+    score = score_fn(league_labels, labels_pred)
+    return score
 
 
 
